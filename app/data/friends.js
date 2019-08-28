7f1b5aa06d13f17d15c407449178f6513d720160
Backslash <lanceadrian13@gmail.com>
2019-08-27 21:08:10 -0400

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,29 @@
 
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
-		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+		name: "Ted",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/1/14/WB0rq.jpg/revision/latest?cb=20130113174836",
+		scores: [4, 3, 3, 2, 3, 2, 3, 2, 4, 4]
 	},
 	{
-		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		name: "Robin",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/9/97/O5wDi.jpg/revision/latest?cb=20150304053526",
+		scores: [1, 5, 5, 3, 5, 5, 5, 5, 2, 2]
 	},
 	{
-		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		name: "Marshall",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/5/50/PlcCt.jpg/revision/latest/scale-to-width-down/210?cb=20130113174817",
+		scores: [4, 2, 2, 3, 2, 1, 4, 2, 5, 3]
 	},
 	{
-		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		name: "Lilly",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/c/cb/JJVSY.jpg/revision/latest?cb=20130113174828",
+		scores: [3, 3, 4, 2, 2, 3, 2, 3, 3, 3]
 	},
 	{
-		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+		name: "Barney",
+		photo: "https://vignette.wikia.nocookie.net/himym/images/8/83/Rc0JG.jpg/revision/latest?cb=20130113174854",
+		scores: [5, 1, 1, 1, 1, 4, 1, 1, 4, 5]
 	}
 ];
 
